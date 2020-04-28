91c9ecc0300ba5815d98112bf33106a24b21ce69
Dianfang Zheng <dfgzheng@thoughtworks.com>
2020-04-28 19:15:02 +0800

    add readme

--- app/src/main/java/com/thoughtworks/xinguanmap/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/xinguanmap/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     private val mapView by lazy { ChinaMapView(this) }
 
-    private val SERVER_URL = ""
+    private val SERVER_URL = "http://api.tianapi.com/txapi/ncovcity/index?key=8cdc508772577994c277514bd9b06cba"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
